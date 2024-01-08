03d0e2d754a2d6ff660d0bbda4d96b263b5cffe0
DH-O <tedoh94@gmail.com>
2024-01-08 14:45:02 +0900

    network ReLU to LReLU

--- model/Networks.py
+++ model/Networks.py
@@ -1,17 +1,22 @@
 import torch.nn as nn
-import torch.nn.functional as F
 
 class QNET(nn.Module):
-    
+
     def __init__(self, input_size, output_size):
         super(QNET, self).__init__()
+        self.LReLU = nn.LeakyReLU(0.01)
         self.fc1 = nn.Linear(input_size, 128)
         self.fc2 = nn.Linear(128, 128)
-        self.fc3 = nn.Linear(128, 128)
-        self.fc4 = nn.Linear(128, output_size)
-        
+        self.fc3 = nn.Linear(128, output_size)
+        self.reset_parameters()
+
+    def reset_parameters(self):
+        nn.init.xavier_uniform_(self.fc1.weight, gain=nn.init.calculate_gain('leaky_relu'))
+        nn.init.xavier_uniform_(self.fc2.weight, gain=nn.init.calculate_gain('leaky_relu'))
+        nn.init.xavier_uniform_(self.fc3.weight, gain=nn.init.calculate_gain('leaky_relu'))
+
     def forward(self, x):
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        x = F.relu(self.fc3(x))
-        return self.fc4(x)
+        x = self.LReLU(self.fc1(x))
+        x = self.LReLU(self.fc2(x))
+        x = self.LReLU(self.fc3(x))
+        return x
